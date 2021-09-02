97b32478935aa8fff4e56f1b27dfb0bacc80d859
Ash Caplan <57927650+accaplan@users.noreply.github.com>
2021-09-02 08:29:14 +0200

    edit to styling

--- src/config.js
+++ src/config.js
@@ -93,16 +93,16 @@ const streetAddress = 'Bulevardi 14';
 const canonicalRootURL = process.env.REACT_APP_CANONICAL_ROOT_URL;
 
 // Site title is needed in meta tags (bots and social media sharing reads those)
-const siteTitle = 'Saunatime';
+const siteTitle = 'Leisure Cooperative';
 
 // Twitter handle is needed in meta tags (twitter:site). Start it with '@' character
-const siteTwitterHandle = '@sharetribe';
+const siteTwitterHandle = null;
 
 // Instagram page is used in SEO schema (http://schema.org/Organization)
-const siteInstagramPage = null;
+const siteInstagramPage = '@leisurecooperative';
 
 // Facebook page is used in SEO schema (http://schema.org/Organization)
-const siteFacebookPage = 'https://www.facebook.com/Sharetribe/';
+const siteFacebookPage = null;
 
 // Social logins & SSO
 
